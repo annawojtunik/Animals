4e8e37bcecf02160bfc91c1d70d26f25810e1a33
Ania <ania@MacBook-Pro-Anna.local>
2017-02-09 03:07:43 +0100

    Initial import

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// plik scripts.js
+
+var	text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach',
+	elephants = 'Zielone słonie',
+	elephants = elephants.toUpperCase(),
+	text = text.replace('Papugi', elephants);
+console.log(text.substr(0, text.length/2));
